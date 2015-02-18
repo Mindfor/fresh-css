98ea6ba37e2a48d8137a406bb3d3a2826a7ca4a8
Alexander Fomin <af@mindfor.ru>
2015-02-18 10:10:16 +0500

    Added minified css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
-var minify = require('gulp-minify-css');
+var minify = require("gulp-minify-css");
+var rename = require("gulp-rename");
 
 gulp.task("less", function () {
 	gulp.src("fresh.less")
 		.pipe(less())
+		.pipe(gulp.dest("./"));
+	
+	gulp.src("fresh.less")
+		.pipe(less())
 		.pipe(minify())
+		.pipe(rename({ suffix: ".min" }))
 		.pipe(gulp.dest("./"));
 });
 
