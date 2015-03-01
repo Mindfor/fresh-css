a092979f4594ac8c3f6ca49ed9f97fa8afc08347
Alexander Fomin <af@mindfor.ru>
2015-03-01 20:17:51 +0500

    Simplified gulpfile and started main layout refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -2,117 +2,73 @@ var gulp = require("gulp");
 var $ = require("gulp-load-plugins")();
 var del = require("del");
 var path = require("path");
-var each = require("foreach");
 var mainBowerFiles = require("main-bower-files");
 
 var paths = {
-	src: "src/",
-	css: "./",
-	vendor: "./vendor/",
-	fonts: "./fonts/"
+	src: "src",
+	dist: "",
+	lib: "lib",
+	fonts: "fonts"
 };
 
 gulp.task("bower:clean", function () {
-	return del(paths.vendor);
+	return del(paths.lib);
 });
 
 gulp.task("bower:install", $.bower);
 
 gulp.task("bower", ["bower:clean", "bower:install"], function () {
-	return gulp.src(mainBowerFiles(), {
-			base: "bower_components"
-		})
-		.pipe(gulp.dest(paths.vendor));
+	return gulp.src(mainBowerFiles(), { base: "bower_components" })
+		.pipe(gulp.dest(paths.lib));
 });
+
 gulp.task("fonts", function () {
-	return gulp
-		.src(paths.vendor + "/font-awesome/fonts/*")
+	return gulp.src(paths.lib + "/font-awesome/fonts/*")
 		.pipe(gulp.dest(paths.fonts));
 });
+
 gulp.task("less", function () {
-	return gulp.src(paths.src + "less/main.less")
+	return gulp.src(paths.src + "/less/main.less")
 		.pipe($.less())
-		.pipe(gulp.dest(paths.css));
-
-	// gulp.src("src/less/main.less")
-	//	.pipe($.less())
-	//  .pipe($.minify())
-	//.pipe($.rename({
-	//  suffix: ".min"
-	//		}))
-	//	  .pipe(gulp.dest("./"));
+		//.pipe($.minifyCss())
+		.pipe(gulp.dest(paths.dist));
 });
 
-var processHtmlsOpts = {
-	process: true,
-	templateSettings: {
-		interpolate: /{{([\s\S]+?)}}/g // mustache
-	},
-	data: {
-		headerTemplate: "",
-		contentTemplate: "",
-		getstarted: "",
-		forms: "",
-		grid: "",
-		buttons: "",
-		helpers: "",
-		labels: "",
-		navigation: "",
-		tables: "",
-		typography: "",
-		blocks: ""
-	}
-}
-
-gulp.task("compile-htmls", function () {
-	return gulp.src(paths.src + "contents/*.cnt.html")
+gulp.task("compile", function () {
+	return gulp.src(paths.src + "/content/*.html")
 		.pipe($.foreach(function (stream, file) {
-			var contentTemplateName = path.basename(file.path);
-
-			var nameWithoutExtensionAndSuffix = contentTemplateName.slice(0, -9);
-
-			//assign active class to property that represent left menu item 
-			each(processHtmlsOpts.data, function (value, key) {
-				if (key == nameWithoutExtensionAndSuffix) {
-					processHtmlsOpts.data[key] = "active";
-				} else {
-					processHtmlsOpts.data[key] = "";
+			var filename = path.basename(file.path);
+			var name = filename.slice(0, -5);
+			
+			var options = {
+				process: true,
+				includeBase: paths.src,
+				templateSettings: {
+					interpolate: /{{([\s\S]+?)}}/g // mustache
+				},
+				data: {
+					header: "header.html",
+					content: filename
 				}
-			});
-			//chose right header depending on whether it start page or others pages
-			if (nameWithoutExtensionAndSuffix == "getstarted") {
-				processHtmlsOpts.data.headerTemplate = "header.html";
-			} else {
-				processHtmlsOpts.data.headerTemplate = "littleheader.html";
 			}
-			processHtmlsOpts.data.contentTemplate = contentTemplateName;
-			return gulp.src(paths.src + "layout.html")
-				.pipe($.processhtml(processHtmlsOpts))
-				.pipe($.rename(nameWithoutExtensionAndSuffix + ".html"))
-				.pipe($.processhtml({
-					includeBase: paths.src
-				}))
-				.pipe($.processhtml({
-					includeBase: paths.src + "contents/"
-				}))
-				.pipe(gulp.dest("./"));
 
-		}))
-})
-
-gulp.task("create-index", function () {
-	return gulp.src("./getstarted.html")
-		.pipe($.rename("index.html"))
-		.pipe(gulp.dest("./"));
-})
-gulp.task("compile", ["compile-htmls"], function () {
-	gulp.start("create-index");
+			// Choose header depending on whether it start page or others pages
+			if (name == "index")
+				options.data.headerTemplate = "header-index.html";
+		
+			return gulp.src(paths.src + "/layout.html")
+				.pipe($.processhtml(options))
+				.pipe($.processhtml({ includeBase: paths.src }))
+				.pipe($.rename(filename))
+				.pipe(gulp.dest(paths.dist));
+		}));
 });
 
-gulp.task("watch", function () {
-	gulp.watch(["src/**"], ["less", "compile"]);
+gulp.task("watch", ["fonts", "less", "compile"], function () {
+	gulp.watch([paths.src + "/**/*.less"], ["less"]);
+	gulp.watch([paths.src + "/**/*.html"], ["compile"]);
 });
 
 gulp.task("default", ["bower"], function () {
-	gulp.start(["watch", "fonts"]);
+	gulp.start("watch");
 });
