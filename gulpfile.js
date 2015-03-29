6ab6bf2fdbeb529c17bd95126d2beeb2bf49255d
Alexander Fomin <af@mindfor.ru>
2015-03-29 11:37:14 +0500

    Added gulp-plumber for error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require("gulp");
+var plumbler = require("gulp-plumber");
 var less = require("gulp-less");
 var minify = require("gulp-minify-css");
 var rename = require("gulp-rename");
 
 gulp.task("less", function () {
 	gulp.src("fresh.less")
+		.pipe(plumbler())
 		.pipe(less())
 		.pipe(gulp.dest("./"));
 	
 	gulp.src("fresh.less")
+		.pipe(plumbler())
 		.pipe(less())
 		.pipe(minify())
 		.pipe(rename({ suffix: ".min" }))
